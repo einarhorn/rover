7ed6276885d1c0b386f155c377f2937483d868c3
eahorn2 <eahorn2@illinois.edu>
2018-02-01 18:03:29 +0000

    Rover base cpp file

---
+++ rover.cpp
@@ -0,0 +1,27 @@
+
+enum Direction {
+  NORTH = 0,
+  EAST = 1,
+  SOUTH = 2,
+  WEST = 3
+};
+
+class Rover {
+public:
+
+
+
+private:
+  // Current row and col of rover
+  int row, col;
+
+  // Direction rover is currently facing
+  Direction dir;
+
+
+};
+
+
+int main() {
+  return 0;
+}
