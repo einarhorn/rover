fe8ddd9e42fe1f25efa676c7515b79b731dab97b
eahorn2 <eahorn2@illinois.edu>
2018-02-01 12:22:32 -0600

    grid class

--- rover.cpp
+++ rover.cpp
@@ -1,6 +1,19 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
+// Represents a 2x2 grid of the planet
+class Grid {
+public:
+  Grid(int numRows, int numCols) {
+    this->numRows = numRows;
+    this->numCols = numCols;
+  }
+  int getNumRows() { return this->numRows; }
+  int getNumCols() { return this->numCols; }
+
+private:
+  int numRows, numCols;
+};
 
 enum Direction {
   NORTH = 0,
@@ -41,26 +54,30 @@ private:
 };
 
 
-// Unit Testing Example
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
-}
-
-
 /**
  * TESTS GO HERE
  **/
 
-// Unit Testing Example
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+// Testing Grid class
+TEST_CASE( "Grid Construction Test", "[grid]" ) {
+    Grid grid = Grid(1, 4);
+    REQUIRE( grid.getNumRows() == 1 );
+    REQUIRE( grid.getNumCols() == 4 );
 }
 
-TEST_CASE( "Rover Construction Test", "[factorial]" ) {
+
+// Testing Rover class
+TEST_CASE( "Rover Construction Test", "[rover]" ) {
     Rover rov = Rover(1, 4);
     REQUIRE( rov.getRow() == 1 );
     REQUIRE( rov.getCol() == 4 );
+}
+
+TEST_CASE( "Rover setters Test", "[rover]" ) {
+    Rover rov = Rover(1, 4);
+    rov.setRow(11);
+    rov.setCol(15);
+    REQUIRE( rov.getRow() == 11 );
+    REQUIRE( rov.getCol() == 15 );
+
 }
